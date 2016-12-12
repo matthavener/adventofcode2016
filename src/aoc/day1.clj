5642b404fa8802a2fdb484ffea4ec2f867e24a1c
Matt Havener <matthavener@gmail.com>
2016-12-11 19:44:53 -0600

    day1p2

--- src/aoc/day1.clj
+++ src/aoc/day1.clj
@@ -10,7 +10,7 @@
    ]
   )
 
-(defn solve []
+(defn vecs []
   (let [cmds (map (fn [c] [(first c)
                            (read-string (apply str (rest c)))])
                   (-> (slurp "day1.input") (string/split #" ")))
@@ -18,12 +18,40 @@
                     (fn [[cur-d ret] [d mag]]
                       (let [next-d (mod ((if (= d \R) dec inc) cur-d)
                                         (count dirs))]
-                        [next-d (conj ret (mapv (partial * mag) (get dirs next-d)))]))
+                        [next-d (conj ret
+                                      (mapv (partial * mag) (get dirs next-d)))]))
                     [0 []] cmds)
 
         ]
-    (+ (apply + (map first ret)) (apply + (map second ret)))))
+    ret
+    ))
+
+(defn solve1 []
+  (let [vecs (vecs)]
+    (+ (apply + (map first vecs) (apply + (map second vecs))))))
+
+(defn unit [n]
+  (cond
+    (= n 0) 0
+    (< n 0) -1
+    :else 1))
+
+(defn solve2 []
+  (let [vecs (vecs)
+        ex-vecs (mapcat (fn [v]
+                          (repeat
+                            (apply + (map #(Math/abs %) v))
+                            (mapv unit v))) vecs)
+        places (reductions #(mapv + %1 %2) [0 0] ex-vecs)
+        duplicates (->> places
+                        (frequencies)
+                        (filter #(> (second %) 1))
+                        (map first)
+                        (into #{}))
+        ]
+    (apply + (map #(Math/abs %) (some duplicates places)))))
 
 (comment
-  (solve)
+  (solve1)
+  (solve2)
   )
